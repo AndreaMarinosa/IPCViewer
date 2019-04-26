524b4e15dce5d8861425c9e68a55b2918156db0d
Andrea <andreamarinosalopez@gmail.com>
2019-04-26 19:28:18 +0200

    Models Common Added

---
+++ IPCViewer.Common/Models/Camera.cs
@@ -0,0 +1,43 @@
+﻿namespace IPCViewer.Common.Models
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class Camera
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("comments")]
+        public string Comments { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("createdDate")]
+        public DateTime CreatedDate { get; set; }
+
+        [JsonProperty("user")]
+        public User User { get; set; }
+
+        [JsonProperty("imageFullPath")]
+        public Uri ImageFullPath { get; set; }
+
+        [JsonProperty("cityId")]
+        public int CityId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Latitude:C6} {this.Longitude:C6}";
+        }
+    }
+}
---
+++ IPCViewer.Common/Models/City.cs
@@ -0,0 +1,14 @@
+﻿namespace IPCViewer.Common.Models
+{
+    using Newtonsoft.Json;
+
+    class City
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+    }
+}
